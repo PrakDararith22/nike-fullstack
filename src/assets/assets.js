e04630e9523c3c794754842757c7a6d63b93718f
member1 <zreaperxu@gmail.com>
2025-07-19 14:27:49 +0700

    chore: setup nessary start file and tool

---
+++ src/assets/assets.js
@@ -0,0 +1,132 @@
+// brand logo
+import porscheIcon from "./brand-logo/porsche-icon.svg";
+import porscheText from "./brand-logo/porsche-icon-text.svg";
+
+// director icons
+import bell from "./icons/indicator/bell.svg";
+import card from "./icons/indicator/card.svg";
+import checkCircle from "./icons/indicator/check-circle.svg";
+import clock from "./icons/indicator/clock.svg";
+import document from "./icons/indicator/document.svg";
+import leather from "./icons/indicator/leather.svg";
+import location from "./icons/indicator/location.svg";
+import mail from "./icons/indicator/mail.svg";
+import pagePinned from "./icons/indicator/page-pinned.svg";
+import profile from "./icons/indicator/profile.svg";
+import tag from "./icons/indicator/tag.svg";
+import truck from "./icons/indicator/truck.svg";
+import heart from "./icons/indicator/heart.svg";
+import heartFilled from "./icons/indicator/heart-filled.svg";
+import cartEmpty from "./icons/indicator/cart-empty.svg";
+import cartFilled from "./icons/indicator/cart-filled.svg";
+import lock from "./icons/indicator/lock.svg";
+import mountain from "./icons/indicator/mountain.svg";
+import car from "./icons/indicator/car.svg";
+import truckFast from "./icons/indicator/truck-fast.svg";
+
+// interface control icons
+import expand from "./icons/interface-controll/expand.svg";
+import filter from "./icons/interface-controll/filter.svg";
+import menu from "./icons/interface-controll/menu.svg";
+import pencil from "./icons/interface-controll/pencil.svg";
+import sliders from "./icons/interface-controll/sliders.svg";
+import sort from "./icons/interface-controll/sort.svg";
+import plus from "./icons/interface-controll/plus.svg";
+import minus from "./icons/interface-controll/minus.svg";
+import cross from "./icons/interface-controll/cross.svg";
+import search from "./icons/interface-controll/search-.svg";
+
+// payments icons
+import americanExpressMd from "./icons/payments/american-express-md.svg";
+import amexSm from "./icons/payments/amex-sm.svg";
+import amexLg from "./icons/payments/amex-lg.svg";
+import applePayLg from "./icons/payments/apple-pay-lg.svg";
+import dinerLg from "./icons/payments/diner-lg.svg";
+import dinerMd from "./icons/payments/diner-md.svg";
+import dinersSm from "./icons/payments/diners-sm.svg";
+import discoverLg from "./icons/payments/discover-lg.svg";
+import discoverMd from "./icons/payments/discover-md.svg";
+import googlePayLg from "./icons/payments/google-pay-lg.svg";
+import googlePaySm from "./icons/payments/google-pay-sm.svg";
+import mastercardLg from "./icons/payments/mastercard-lg.svg";
+import paypalLg from "./icons/payments/paypal-lg.svg";
+import paypalSm from "./icons/payments/paypal-sm.svg";
+import paypalMd from "./icons/payments/paypal-md.svg";
+import visaLg from "./icons/payments/visa-lg.svg";
+import visaMd from "./icons/payments/visa-md.svg";
+
+// social media icons
+import facebook from "./icons/social-media/facebook.svg";
+import google from "./icons/social-media/google.svg";
+import instagram from "./icons/social-media/instagram.svg";
+import linkin from "./icons/social-media/linkin.svg";
+import pinterest from "./icons/social-media/pinterest.svg";
+import telegram from "./icons/social-media/telegram.svg";
+import x from "./icons/social-media/x.svg";
+import youtube from "./icons/social-media/youtube.svg";
+
+// navigation
+import arrow from "./icons/navigation/arrow.svg";
+import chevron from "./icons/navigation/chevron.svg";
+
+export {
+  expand,
+  filter,
+  menu,
+  pencil,
+  sliders,
+  sort,
+  americanExpressMd,
+  amexSm,
+  amexLg,
+  applePayLg,
+  dinerLg,
+  dinerMd,
+  dinersSm,
+  discoverLg,
+  discoverMd,
+  googlePayLg,
+  googlePaySm,
+  mastercardLg,
+  paypalLg,
+  paypalSm,
+  paypalMd,
+  visaLg,
+  visaMd,
+  facebook,
+  google,
+  instagram,
+  linkin,
+  pinterest,
+  telegram,
+  x,
+  youtube,
+  arrow,
+  chevron,
+  porscheIcon,
+  porscheText,
+  minus,
+  plus,
+  cross,
+  bell,
+  card,
+  checkCircle,
+  clock,
+  document,
+  leather,
+  location,
+  mail,
+  pagePinned,
+  profile,
+  tag,
+  truck,
+  heart,
+  heartFilled,
+  search,
+  cartFilled,
+  cartEmpty,
+  lock,
+  truckFast,
+  mountain,
+  car,
+};
