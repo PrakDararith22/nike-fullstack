c6f9fe84a229de24a9cc44d42768f174f2614bca
Dararith <prakdararith22@gmail.com>
2025-07-24 12:49:46 +0700

    feat: implement the auth page for all auth page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { homePage } from "./home-page/home-page.js";
 export { productDetail } from "./product-detail/product-detail.js";
 export { productList } from "./product-list/product-list.js";
+export { authPage } from "./auth/auth.js";
