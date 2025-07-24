46ad05c411f3dde8ed6cc60623fcd8650f9d5256
Dararith <prakdararith22@gmail.com>
2025-07-24 09:50:47 +0700

    add: event emitter and disabled feature to search-input

--- src/components/base/search-input/search-input.js
+++ src/components/base/search-input/search-input.js
@@ -2,17 +2,41 @@ import { search } from "@assets";
 
 class searchInput extends BaseComponent {
   static get observedAttributes() {
-    return ["text"];
+    return ["text", "disabled", "action"];
   }
 
   constructor() {
     super();
     this.text = "";
+    this.disabled = false;
+    this.action = "";
   }
 
   connectedCallback() {
     super.connectedCallback();
     this.updateTemplate();
+    this.addEventListener("click", event => {
+      if (this.disabled) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
+      this.emitAction();
+    });
+  }
+
+  emitAction() {
+    console.log("search input here");
+    if (this.disabled || !this.action) return;
+    this.dispatchEvent(
+      new CustomEvent("search-input", {
+        bubbles: true,
+        composed: true,
+        detail: {
+          action: this.action,
+        },
+      })
+    );
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -20,6 +44,12 @@ class searchInput extends BaseComponent {
       switch (name) {
         case "text":
           this.text = newValue || "";
+          break;
+        case "action":
+          this.action = newValue || "";
+          break;
+        case "disabled":
+          this.disabled = newValue !== null;
           break;
         default:
           console.warn(`Unhandled observed attribute: ${name}`);
@@ -32,8 +62,8 @@ class searchInput extends BaseComponent {
   updateTemplate() {
     this.template = `
     <div class="search-input bg-gray-200 flex items-center px-3 rounded-lg w-full">
-      <icon-button icon="${search}"></icon-button>
-      <input type="text" placeholder="${this.text}" 
+      <icon-button icon="${search}" "></icon-button>
+      <input type="text" placeholder="${this.text} ${this.disabled ? "disabled" : ""}" 
       class="outline-none border-none body-sm w-full bg-transparent text-p1 font-semibold text-gray-500"/>
     </div>
     `;
