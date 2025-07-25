9b1a62c8388fd59830fae2f6a956e8a9aa6b8b6c
Dararith <prakdararith22@gmail.com>
2025-07-25 21:36:11 +0700

    add: product json data and image

--- src/components/menu-panel/menu-panel.js
+++ src/components/menu-panel/menu-panel.js
@@ -52,12 +52,12 @@ class MenuPanel extends BaseComponent {
             <div class="flex flex-col w-full">
                 <icon-button icon="${cross}" action="menuClose" class="align-self-end "></icon-button>
                 <div class="pt-10 flex flex-col gap-4 text-h3 font-semibold">
-                    <a>New & Featured</a>
-                    <a>Men</a>
-                    <a>Women</a>
-                    <a>Kids</a>
-                    <a>Jordan</a>
-                    <a>Sale</a>
+                    <a href="new-and-featured">New & Featured</a>
+                    <a href="Men">Men</a>
+                    <a href="Women">Women</a>
+                    <a href="Kids">Kids</a>
+                    <a href="Jordan">Jordan</a>
+                    <a href="Sale">Sale</a>
                 </div>
             </div>
 
@@ -67,13 +67,13 @@ class MenuPanel extends BaseComponent {
                     <a class="inline text-black font-bold"> <u> Learn More </u></a>
                 </div>
                 <div class=" pb-12 mb-12  flex gap-2">
-                    <base-button text="Join Us"></base-button>
-                    <base-button text="Sign In" variant="secondary"></base-button>
+                    <base-button text="Join Us" ></base-button>
+                    <base-button text="Sign In" variant="secondary" to="/signin"></base-button>
                 </div>
 
                 <div class="flex flex-col gap-3">
-                    <icon-button icon="${heart}" text="Favorite"></icon-button>
-                    <icon-button icon="${cartEmpty}" text="Bag"></icon-button>
+                    <icon-button icon="${heart}" text="Favorite" to="/favorite"></icon-button>
+                    <icon-button icon="${cartEmpty}" text="Bag" to="/cart"></icon-button>
                     <icon-button icon="${heart}" text="Track Order"></icon-button>
                     <icon-button icon="${heart}" text="Find a Store"></icon-button>
                     <icon-button icon="${heart}" text="Help"></icon-button>
