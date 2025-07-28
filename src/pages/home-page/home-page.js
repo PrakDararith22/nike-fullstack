69062fac4d7428b5ae0c1a1980d7477df60fe6de
Dararith <prakdararith22@gmail.com>
2025-07-28 21:15:11 +0700

    fix: home page responsive spacing banner image and link to pages

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -1,80 +1,113 @@
 import "@components";
 import "./home-page.css";
+import { shuffleObjectValues } from "@utility";
+import { banner, banner2, banner3, banner4, banner5 } from "../../assets/banner";
 
-export function homePage() {
+export function homePage(product) {
+  const showcaseCards = [
+    {
+      to: "women",
+      image: banner,
+      title: "Nike National Team Collections",
+      description: "Fear Nothing",
+      layout: "bottomleft",
+      cta: "shop",
+    },
+    {
+      to: "man",
+      image: banner2,
+      title: "Total 90",
+      description: "Look of Football",
+      layout: "bottomleft",
+      cta: "shop",
+    },
+    {
+      to: "kids",
+      image: banner4,
+      title: "Turn Offseason On",
+      description: "Summer Ready",
+      layout: "bottomleft",
+      cta: "shop",
+    },
+    {
+      to: "jordan",
+      image: banner3,
+      title: "Structure 26",
+      description: "Just In",
+      layout: "bottomleft",
+      cta: "shop",
+    },
+  ];
   return /* html */ `
-    <div class="page home-page flex flex-col gap-2">
-       
-        <base-header></base-header>
-        <top-message-bar linktext="Join us!"></top-message-bar>
-
-        <!-- hero section -->
-        <showcase-card
-            image="src/assets/banner.png"
-            title="sfs"
-            description="sdsdf"
-            layout="bottomcenter"
-            cta="shop"
-        ></showcase-card>
-
-        <!-- product grid -->
-        <div class="product-grid">
-            <showcase-card
-            image="src/assets/banner.png"
-            title="sfs"
-            description="sdsdf"
-            layout="bottomcenter"
-            cta="shop"
-            ></showcase-card>
-            <showcase-card
-            image="src/assets/banner.png"
-            title="sfs"
-            description="sdsdf"
-            layout="bottomcenter"
-            cta="shop"
-            ></showcase-card>
-            <showcase-card
-            image="src/assets/banner.png"
-            title="sfs"
-            description="sdsdf"
-            layout="bottomcenter"
-            cta="shop"
-            ></showcase-card>
-            <showcase-card
-            image="src/assets/banner.png"
-            title="sfs"
-            description="sdsdf"
-            layout="bottomcenter"
-            cta="shop"
-            ></showcase-card>
+    <div>
+        <div>
+          <base-header></base-header>
+          <top-message-bar linktext="Join us!"></top-message-bar>
         </div>
-        <!-- spotlight section -->
-        <showcase-card
-            image="src/assets/banner.png"
-            title="sfs"
-            description="sdsdf"
-            layout="bottomcenter"
-            cta="shop"
-        ></showcase-card>
+            <a href="new-and-featured">
+            <showcase-card
+                image="${banner2}"
+                title="HOME AGAIN"
+                description="Celebrate the Lionesses' histroric second win."
+                layout="bottomleft"
+                cta="shop"
+                type2= "lifestyle"
+            ></showcase-card>
+          </a>
+          <div class="home-page">
+              <div class="home-page-product-grid">
+                  ${showcaseCards
+                    .map(
+                      item => `
+                            <a href="${item.to}">
+                                <showcase-card
+                                    image="${item.image}"
+                                    title="${item.title}"
+                                    description="${item.description}"
+                                    layout="${item.layout}"
+                                    cta="${item.cta}"
+                                    ${item.type2 ? `type2="${item.type2}"` : ""}
+                                    ${item.color ? `color="${item.color}"` : ""}
+                                ></showcase-card>
+                            </a>`
+                    )
+                    .join("")}
+              </div>
 
-        <!-- product show case -->
-        <div 
-        class="product-show-case  whitespace-nowrap  pt-8 pl-5 ">
-        <h2 class="font-medium "> Shop Our Icons </h2>
-            <div class="pt-5 overflow-x-auto scrollbar-hidden">
-                <showcase-card 
-                class="product-grid-card inline-block mr-2"
-                image="src/assets/banner.png" 
-                layout="bottomcenter" 
-                cta="shop"
-                ></showcase-card>
-                <showcase-card 
-                class=" product-grid-card inline-block mr-2"
-                image="src/assets/banner.png" 
-                layout="bottomcenter" 
-                cta="shop"
-                ></showcase-card>
-            </div>
+                <a href="jordan">
+                  <showcase-card
+                      image="${banner5}"
+                      title="Athlete Picks"
+                      description="Kylian Mbappe"
+                      layout="bottomleft"
+                      cta="shop"
+                      type2= "lifestyle"
+                  ></showcase-card>
+                </a>
+
+                <!-- product show case -->
+                <div class=" home-page-icon-container whitespace-nowrap ">
+                  <h2 class="font-medium "> Shop Our Icons </h2>
+                    <div class="pt-5 overflow-x-auto scrollbar-hidden">
+                       ${shuffleObjectValues(product)
+                         .map(
+                           item => `
+                           <a href="${item.gender}/${item.id}">
+                                <showcase-card
+                                class="home-page-product-grid-card inline-block mr-2"
+                                image="${item.images}"
+                                layout="bottomcenter"
+                                cta="shop"
+                                type2="lifestyle"
+                                ></showcase-card>
+                            <a>`
+                         )
+                         .join("")}
+                    </div>
+                </div>
+
+          </div>   
+
         </div>
 
         <base-footer></base-footer>
