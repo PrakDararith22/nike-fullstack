025a3618f0ea3f3a13e5057e6722af68231aef22
Dararith <prakdararith22@gmail.com>
2025-08-22 17:58:02 +0700

    add: implement user express valdiation

--- server/middleware/userValidator.js
+++ server/middleware/userValidator.js
@@ -3,7 +3,7 @@ import { body } from "express-validator";
 const nameRegex = /^[A-Za-z0-9]+$/i;
 const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
-const registerValidation = [
+export const registerValidation = [
   body("username")
     .notEmpty()
     .withMessage("Username is required")
@@ -28,5 +28,38 @@ const registerValidation = [
     .isLength({ min: 8 })
     .withMessage("Password too short"),
 ];
+export const loginValidation = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .bail()
+    .isLength({ min: 8 })
+    .withMessage("Email must be at least 8 characters")
+    .bail()
+    .matches(emailRegex)
+    .withMessage("Invalid Email"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .bail()
+    .isLength({ min: 8 })
+    .withMessage("Password too short"),
+];
 
-export default registerValidation;
+export const verifyOptValidation = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .bail()
+    .isLength({ min: 8 })
+    .withMessage("Email must be at least 8 characters")
+    .bail()
+    .matches(emailRegex)
+    .withMessage("Invalid Email"),
+  body("opt")
+    .notEmpty()
+    .withMessage("OPT Required")
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage("OPT Atlest 6 Character"),
+];
